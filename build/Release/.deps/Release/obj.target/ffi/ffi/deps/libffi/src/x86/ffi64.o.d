d3f423276d1f488cf8857d36fb6802d7dfe81638
Tim Cameron Ryan <id@timryan.org>
2014-01-15 23:42:55 -0500

    Initial commit

---
+++ build/Release/.deps/Release/obj.target/ffi/ffi/deps/libffi/src/x86/ffi64.o.d
@@ -0,0 +1,12 @@
+cmd_Release/obj.target/ffi/ffi/deps/libffi/src/x86/ffi64.o := cc '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DPIC' '-DFFI_BUILDING' '-DHAVE_CONFIG_H' '-DNDEBUG' -I/Users/tim/.node-gyp/0.10.18/src -I/Users/tim/.node-gyp/0.10.18/deps/uv/include -I/Users/tim/.node-gyp/0.10.18/deps/v8/include -I../ffi/deps/libffi/include -I../ffi/deps/libffi/config/mac/x64  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/ffi/ffi/deps/libffi/src/x86/ffi64.o.d.raw  -c -o Release/obj.target/ffi/ffi/deps/libffi/src/x86/ffi64.o ../ffi/deps/libffi/src/x86/ffi64.c
+Release/obj.target/ffi/ffi/deps/libffi/src/x86/ffi64.o: \
+  ../ffi/deps/libffi/src/x86/ffi64.c \
+  ../ffi/deps/libffi/config/mac/x64/ffi.h \
+  ../ffi/deps/libffi/config/mac/x64/ffitarget.h \
+  ../ffi/deps/libffi/include/ffi_common.h \
+  ../ffi/deps/libffi/config/mac/x64/fficonfig.h
+../ffi/deps/libffi/src/x86/ffi64.c:
+../ffi/deps/libffi/config/mac/x64/ffi.h:
+../ffi/deps/libffi/config/mac/x64/ffitarget.h:
+../ffi/deps/libffi/include/ffi_common.h:
+../ffi/deps/libffi/config/mac/x64/fficonfig.h:
---
+++ build/Release/.deps/Release/obj.target/ffi_bindings/ffi/src/callback_info.o.d
@@ -0,0 +1,29 @@
+cmd_Release/obj.target/ffi_bindings/ffi/src/callback_info.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/tim/.node-gyp/0.10.18/src -I/Users/tim/.node-gyp/0.10.18/deps/uv/include -I/Users/tim/.node-gyp/0.10.18/deps/v8/include -I../ffi/deps/libffi/include -I../ffi/deps/libffi/config/mac/x64  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-threadsafe-statics -fno-strict-aliasing -ObjC++ -MMD -MF ./Release/.deps/Release/obj.target/ffi_bindings/ffi/src/callback_info.o.d.raw  -c -o Release/obj.target/ffi_bindings/ffi/src/callback_info.o ../ffi/src/callback_info.cc
+Release/obj.target/ffi_bindings/ffi/src/callback_info.o: \
+  ../ffi/src/callback_info.cc \
+  /Users/tim/.node-gyp/0.10.18/src/node_buffer.h \
+  /Users/tim/.node-gyp/0.10.18/src/node.h \
+  /Users/tim/.node-gyp/0.10.18/deps/uv/include/uv.h \
+  /Users/tim/.node-gyp/0.10.18/deps/uv/include/uv-private/uv-unix.h \
+  /Users/tim/.node-gyp/0.10.18/deps/uv/include/uv-private/ngx-queue.h \
+  /Users/tim/.node-gyp/0.10.18/deps/uv/include/uv-private/uv-darwin.h \
+  /Users/tim/.node-gyp/0.10.18/deps/v8/include/v8.h \
+  /Users/tim/.node-gyp/0.10.18/deps/v8/include/v8stdint.h \
+  /Users/tim/.node-gyp/0.10.18/src/node_object_wrap.h \
+  /Users/tim/.node-gyp/0.10.18/src/node_version.h ../ffi/src/ffi.h \
+  ../ffi/deps/libffi/config/mac/x64/ffi.h \
+  ../ffi/deps/libffi/config/mac/x64/ffitarget.h
+../ffi/src/callback_info.cc:
+/Users/tim/.node-gyp/0.10.18/src/node_buffer.h:
+/Users/tim/.node-gyp/0.10.18/src/node.h:
+/Users/tim/.node-gyp/0.10.18/deps/uv/include/uv.h:
+/Users/tim/.node-gyp/0.10.18/deps/uv/include/uv-private/uv-unix.h:
+/Users/tim/.node-gyp/0.10.18/deps/uv/include/uv-private/ngx-queue.h:
+/Users/tim/.node-gyp/0.10.18/deps/uv/include/uv-private/uv-darwin.h:
+/Users/tim/.node-gyp/0.10.18/deps/v8/include/v8.h:
+/Users/tim/.node-gyp/0.10.18/deps/v8/include/v8stdint.h:
+/Users/tim/.node-gyp/0.10.18/src/node_object_wrap.h:
+/Users/tim/.node-gyp/0.10.18/src/node_version.h:
+../ffi/src/ffi.h:
+../ffi/deps/libffi/config/mac/x64/ffi.h:
+../ffi/deps/libffi/config/mac/x64/ffitarget.h:
